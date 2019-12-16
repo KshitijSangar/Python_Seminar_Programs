669e2eeca6bb18509d4470a53a53142a50d9e9d6
Kshitij Sangar <kshitijsangar@gmail.com>
2019-12-16 21:07:41 +0530

    This is the File

---
+++ creating_game_loop.py
@@ -0,0 +1,16 @@
+import pygame
+
+x = pygame.init()
+print(x)
+gamewindow = pygame.display.set_mode((1000,600))
+pygame.display.set_caption("hello")
+
+
+exit_game = False
+game_over = False
+
+#Creating Game Loop
+while not exit_game:
+    pass
+pygame.quit()
+quit()
